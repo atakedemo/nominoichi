b6985ee85e06021a67f6924bf5461980d8481e05
admin02-at-594175341170 <70833127+atakedemo@users.noreply.github.com>
2025-03-08 14:54:07 +0900

    Hardhatのデプロイ設定

--- nominoichi-contract/hardhat.config.ts
+++ nominoichi-contract/hardhat.config.ts
@@ -1,8 +1,34 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import { vars } from "hardhat/config";
+
+const PRIVATE_KEY = vars.get("PRIVATE_KEY");
+const ALCHEMY_API_KEY = vars.get("ALCHEMY_API_KEY");
+const ETHERSCAN_API_KEY = vars.get("ETHERSCAN_API_KEY");
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  networks: {
+    baseSepolia: {
+      url: `https://base-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      baseSepolia: ETHERSCAN_API_KEY,
+    },
+    customChains: [
+      {
+        network: "baseSepolia",
+        chainId: 84532,
+        urls: {
+          apiURL: "https://api-sepolia.basescan.org/api",
+          browserURL: "https://sepolia.basescan.org"
+        }
+      }
+    ]
+  },
 };
 
 export default config;
--- nominoichi-contract/ignition/modules/OrderNft.ts
+++ nominoichi-contract/ignition/modules/OrderNft.ts
@@ -1,17 +1,14 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
-// const JAN_1ST_2030 = 1893456000;
-// const ONE_GWEI: bigint = 1_000_000_000n;
+const USDC_ADDRESS = "0x036CbD53842c5426634e7929541eC2318f3dCF7e"
 
 const LockModule = buildModule("OrderNftModule", (m) => {
-//   const unlockTime = m.getParameter("unlockTime", JAN_1ST_2030);
-//   const lockedAmount = m.getParameter("lockedAmount", ONE_GWEI);
 
   const OrderNft = m.contract("OrderNft", [], {
     // value: lockedAmount,
   });
-  m.call(OrderNft, "initialize", ["OrderNft", "oNFT"]);
-  m.call(OrderNft, "mint", ["0x7b718D4Ce6ca83536660a314639559F3d3f6e9e3"]);
+  m.call(OrderNft, "initialize", [USDC_ADDRESS]);
+  m.call(OrderNft, "mint", ["0x7b718D4Ce6ca83536660a314639559F3d3f6e9e3", 1]);
 
   return {OrderNft };
 });
