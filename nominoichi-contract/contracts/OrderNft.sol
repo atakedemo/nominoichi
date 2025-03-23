0133ef19453abef3fc5fc00bbe016f55957a0702
admin02-at-594175341170 <70833127+atakedemo@users.noreply.github.com>
2025-03-24 08:17:17 +0900

    一括購入対応のためERC1155へ移行

--- nominoichi-contract/contracts/OrderNft.sol
+++ nominoichi-contract/contracts/OrderNft.sol
@@ -1,19 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC1155/ERC1155Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {IPermit} from "./interfaces/IPermit.sol";
 
-contract OrderNft is Initializable, ERC721Upgradeable, OwnableUpgradeable, UUPSUpgradeable {
+contract OrderToken is Initializable, ERC1155Upgradeable, OwnableUpgradeable, UUPSUpgradeable {
     event Error(bytes errMessage);
     event Permit(address owner, address spender, uint256 amount, uint256 deadline, uint8 v, bytes32 r, bytes32 s);
+    event Purchase(address consumer, uint256[] tokenIds, uint256[] amounts, uint256 feeTotal);
+
     struct Product {
         uint256 tokenId;
         uint256 price; 
+        uint256 stock; 
         uint8 status;
         address owner;
     }
@@ -27,25 +30,42 @@ contract OrderNft is Initializable, ERC721Upgradeable, OwnableUpgradeable, UUPSU
     //Logic
     //==========================
     function initialize(address tAddress) public initializer {
-        __ERC721_init("OrderNft", "oNFT");
         __Ownable_init(msg.sender);
         __UUPSUpgradeable_init();
         usdcAddress = tAddress;
     }
 
-    function purchase(uint256 tokenId, uint256 deadline, uint8 v, bytes32 r, bytes32 s) public returns (uint256) {
-        uint256 _fee = products[tokenId].price;
-        address _owner = products[tokenId].owner;
-        IPermit permitToken = IPermit(usdcAddress);
-        try permitToken.permit(msg.sender, address(this), _fee, deadline, v, r, s) {
-            _safeMint(msg.sender, tokenId);
-            IERC20 usdc = IERC20(usdcAddress);
-            usdc.transferFrom(msg.sender, address(this), _fee);
+    function purchase(
+        uint256[] memory tokenIds, 
+        uint256[] memory amounts, 
+        uint256 permitFee,
+        uint256 deadline, 
+        uint8 v, 
+        bytes32 r, 
+        bytes32 s
+    ) public returns (uint256) {
+        uint256 _feeTotal = 0;
+        for (uint256 i = 0; i < tokenIds.length; ++i) {
+            uint256 _id = tokenIds[i];
+            uint256 _fee = products[_id].price;
+            _feeTotal += _fee;
+
+            address _owner = products[_id].owner;
             withdrawableFee[_owner] += _fee;
-            return tokenId;
+        }
+        
+        IPermit permitToken = IPermit(usdcAddress);
+        try permitToken.permit(msg.sender, address(this), permitFee, deadline, v, r, s) {
+            _mintBatch(msg.sender, tokenIds, amounts, '0x0');
+            IERC20 usdc = IERC20(usdcAddress);
+            usdc.transferFrom(msg.sender, address(this), _feeTotal);
+            emit Purchase(msg.sender, tokenIds, amounts, _feeTotal);
+            
+            return _feeTotal;
         } catch (bytes memory errMessage) {
-            emit Permit(msg.sender, address(this), _fee, deadline, v, r, s);
+            emit Permit(msg.sender, address(this), _feeTotal, deadline, v, r, s);
             emit Error(errMessage);
+            return 0;
         }
     }
 
@@ -63,12 +83,14 @@ contract OrderNft is Initializable, ERC721Upgradeable, OwnableUpgradeable, UUPSU
     function listProduct(
         uint256 tokenId,
         uint256 price,
+        uint256 stock,
         uint8 status
     ) external onlyOwner{
         require(status < 3, "Invalid status");
         products[tokenId] = Product({
             tokenId: tokenId,
             price: price,
+            stock: stock,
             status: status,
             owner: msg.sender
         });
@@ -81,12 +103,34 @@ contract OrderNft is Initializable, ERC721Upgradeable, OwnableUpgradeable, UUPSU
         products[tokenId].price = price;
     }
 
-    function setProductStatus(
+    function updateStatus(
         uint256 tokenId,
         uint8 status
-    ) external onlyOwner{
+    ) external {
+        address _owner = products[tokenId].owner;
         require(status < 3, "Invalid status");
+        require(msg.sender == _owner, "You are not owner");
         products[tokenId].status = status;
+    }
+
+    function addStock(
+        uint256 tokenId,
+        uint256 amount
+    ) external {
+        address _owner = products[tokenId].owner;
+        require(msg.sender == _owner, "You are not owner");
+        products[tokenId].stock += amount;
+    }
+
+    function reduceStock(
+        uint256 tokenId,
+        uint256 amount
+    ) external {
+        address _owner = products[tokenId].owner;
+        uint256 _stock = products[tokenId].stock;
+        require(msg.sender == _owner, "You are not owner");
+        require(_stock - amount >= 0, "Stock is too low");
+        products[tokenId].stock -= amount;
     }
 
     function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
--- nominoichi-contract/ignition/modules/OrderNft.ts
+++ nominoichi-contract/ignition/modules/OrderNft.ts
@@ -2,14 +2,14 @@ import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 const USDC_ADDRESS = "0x036CbD53842c5426634e7929541eC2318f3dCF7e"
 
-const LockModule = buildModule("OrderNftModule", (m) => {
+const LockModule = buildModule("OrderTokenModule", (m) => {
 
-  const OrderNft = m.contract("OrderNft", [], {
+  const OrderToken = m.contract("OrderToken", [], {
     // value: lockedAmount,
   });
-  m.call(OrderNft, "initialize", [USDC_ADDRESS]);
+  m.call(OrderToken, "initialize", [USDC_ADDRESS]);
 
-  return {OrderNft };
+  return {OrderToken };
 });
 
 export default LockModule;
